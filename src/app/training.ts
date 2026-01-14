aaaf96ee909a6094924847f3f7e11cd7b26b5e58
khabbin <hvzalala@gmail.com>
2026-01-14 22:20:01 +0500

    fix issues

--- src/app/training.ts
+++ src/app/training.ts
@@ -1,5 +1,10 @@
+enum SubscriptionPlan {
+  Monthly = 'MONTHLY',
+  Yearly = 'YEARLY'
+}
+
 interface IPremiumUser extends IUser {
-  subscriptionPlan: 'monthly' | 'yearly';
+  subscriptionPlan: SubscriptionPlan;
   premiumSince: Date;
 }
 
@@ -14,41 +19,65 @@ const kirill: IPremiumUser = {
   id: 1,
   name: 'kirill',
   password: 123,
-  subscriptionPlan: 'monthly',
+  subscriptionPlan: SubscriptionPlan.Monthly,
   premiumSince: new Date()
 }
 
 export const users: IUser[] = [
   kirill,
-  {id: 2, password: 123, name: 'хайтаб'},
-  {id: 3, password: 444, name: 'killer67'},
-  {id: 4, password: 12345, name: 'артем'},
-  {id: 5, password: 222222222, name: 'лоутаб'},
+  {
+    id: 2,
+    password: 123,
+    name: 'хайтаб'
+  },
+  {
+    id: 3,
+    password: 444,
+    name: 'killer67'
+  },
+  {
+    id: 4,
+    password: 12345,
+    name: 'артем'
+  },
+  {
+    id: 5,
+    password: 222222222,
+    name: 'лоутаб'
+  }
 ];
 
 export const filteredUsers = users.filter(user => user.id > 2)
 
-const getSumm = (a: number, b: number): number => {
+const getSum = (a: number, b: number): number => {
   return a + b;
 }
 
 export const transformText = (string: string, textCase: TextFormat): string => {
-  if (textCase === "uppercase") {
-    return string.toUpperCase();
+  switch(textCase) {
+    case TextFormat.UpperCase:
+      return string.toUpperCase();
+    case TextFormat.LowerCase:
+      return string.toLowerCase();
+    case TextFormat.Capitalize:
+      return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    default:
+      return string;
   }
-  if (textCase === "lowercase") {
-    return string.toLowerCase();
-  }
-  if (textCase === "capitalize") {
-    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-  }
-  return string;
 }
 
-export const deleteSymbol = (text: string, symbol: unknown): string => {
-  const target = String(symbol);
-  return text.replaceAll(target, '');
+export const deleteSymbol = (text: string, symbol: string): string => {
+  return text.replaceAll(symbol, '');
 }
 
-export let status: "loading" | "success" | "error";
-export type TextFormat = "uppercase" | "lowercase" | "capitalize";
+enum Status {
+  Loading = 'LOADING',
+  Success = 'SUCCES',
+  Error = 'ERROR'
+}
+
+enum TextFormat {
+  UpperCase = 'UPPERCASE',
+  LowerCase = 'LOWERCASE',
+  Capitalize = 'CAPITALIZE'
+}
