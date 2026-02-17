922df5ad686fcd2f73ace531d04f0d446acce5c3
khabbin <hvzalala@gmail.com>
2026-02-17 16:20:02 +0500

    shortcomings

--- src/app/local-storage.service.ts
+++ src/app/local-storage.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class LocalStorageService {
+  
   setItem<T>(key: string, value: T): void {
     localStorage.setItem(key, JSON.stringify(value));
   }
